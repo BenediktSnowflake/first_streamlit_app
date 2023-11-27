ce8e423fbf68c40c13165f3dd2aa1522deabe853
BenediktSnowflake <151635341+BenediktSnowflake@users.noreply.github.com>
2023-11-27 12:29:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 
 import streamlit
 import pandas
-
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 streamlit.title('🥣 🥗 🐔 🥑🍞')
 
 streamlit.header('Breakfast Menu')
